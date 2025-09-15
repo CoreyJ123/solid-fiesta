4e494adfa4296948b9350c453db12deac16e2f4b
Olivia Milton (1000137301) <MILTOL1@student.op.ac.nz>
2025-09-16 10:58:13 +1200

    haha merge conflict

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿namespace StudioTest
+﻿using System;
+
+namespace StudioTest
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+
+
+
+
+            Random rand = new Random();
+            int haha = rand.Next(1,11);
+            Console.WriteLine("Press Enter to get a random number");
+            Console.WriteLine(haha);
+            Console.WriteLine("Wow thats pretty cool");
+            Console.WriteLine("Merge Conflict yayayayayay");
         }
     }
 }
