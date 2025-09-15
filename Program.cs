5fafd4b1e1621572f330e384d49971eddd8949a4
Olivia Milton (1000137301) <MILTOL1@student.op.ac.nz>
2025-09-16 10:45:04 +1200

    Studio test

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StudioTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
